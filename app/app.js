dda2e42cbb928868f866b276ac39451f62ac4c13
nikthequik <quik.the.nick@gmail.com>
2016-08-14 21:47:08 -0600

    -create navbar -month and year selects

--- app/app.js
+++ app/app.js
@@ -1,3 +1,31 @@
-angular.module('calendarDemoApp', []);
+angular.module('calendarDemoApp', [])
 
 // your controller and directive code go here
+.controller('calendarController', ['$scope', function($scope) {
+	$scope.yearRange = [];
+	var yearInc = 1996;
+	while (yearInc < 2037) {
+		$scope.yearRange.push(yearInc);
+		yearInc++;
+	};
+	$scope.months = [
+		'January', 
+		'February', 
+		'March', 
+		'April', 
+		'May', 
+		'June', 
+		'July', 
+		'August', 
+		'September', 
+		'October', 
+		'November', 
+		'December'
+	];
+}])
+.directive('calendarDirective', function() {
+	return {
+		scope: true,
+		
+	}
+});
