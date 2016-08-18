2fd6a7655dc402fb858d24c0f6e9b7a2d06912a0
nikthequik <quik.the.nick@gmail.com>
2016-08-17 23:10:13 -0600

    build app and add slight styling

--- app/app.js
+++ app/app.js
@@ -1,27 +1,85 @@
-angular.module('calendarDemoApp', [])
+angular.module('calendarDemoApp')
 
 // your controller and directive code go here
-.controller('calendarController', ['$scope', function($scope) {
+.controller('calendarController', ['$scope', 'CalendarRange', function($scope, CalendarRange) {
 	$scope.yearRange = [];
-	var yearInc = 1996;
-	while (yearInc < 2037) {
-		$scope.yearRange.push(yearInc);
+	$scope.week = [];
+	var yearInc = 0;
+	var yearStart = 1996;
+	var weekInc = 0;
+	while (yearInc <= 40) {
+		$scope.yearRange.push(yearInc + yearStart);
 		yearInc++;
 	};
 	$scope.months = [
-		'January', 
-		'February', 
-		'March', 
-		'April', 
-		'May', 
-		'June', 
-		'July', 
-		'August', 
-		'September', 
-		'October', 
-		'November', 
-		'December'
+		{
+			name: 'January',
+			value: 0
+		},
+		{
+			name: 'February',
+			value: 1
+		}, 
+		{
+			name: 'March',
+			value: 2
+		}, 
+		{
+			name: 'April',
+			value: 3
+		}, 
+		{
+			name: 'May',
+			value: 4
+		}, 
+		{
+			name: 'June',
+			value: 5
+		},  
+		{
+			name: 'July',
+			value: 6
+		}, 
+		{
+			name: 'August',
+			value: 7
+		}, 
+		{
+			name: 'September',
+			value: 8
+		}, 
+		{
+			name: 'October',
+			value: 9
+		}, 
+		{
+			name: 'November',
+			value: 10
+		}, 
+		{
+			name: 'December',
+			value: 11
+		}, 
 	];
+
+	$scope.selected = {
+		month: null,
+		year: null,
+	};
+
+	$scope.changeDate = function () {
+		console.log($scope.selected.month.value)
+		if ($scope.selected.month !== null && $scope.selected.year !== null) {
+			$scope.month = CalendarRange.getMonthlyRange(new Date($scope.selected.month.value + 1 + '/1/' + $scope.selected.year));
+		}
+	}
+
+	while (weekInc) {
+		$scope.week.push({
+			currentWeek: false
+		});
+	}
+	console.log(new Date());
 }])
 .directive('calendarDirective', function() {
 	return {
